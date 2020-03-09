b5bc9f5e4174abb1595fb09f34cc760682b5e545
zhougaojun <zhougaojun@dxy.cn>
2020-03-09 21:18:48 +0800

    add file

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author zhougaojun
+ */
 @RestController
 public class HelloController {
 
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 /**
  * @author zhougaojun
  */
+@Slf4j
 public class AppTest {
+
+    @Test
+    public void test(){
+
+        log.info("test !");
+    }
 }
