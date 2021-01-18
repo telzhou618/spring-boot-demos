5b6ab19ec1f6aff92bcb784cd3cc06d7af12b12f
zhougaojun <zhougaojun@dxy.cn>
2021-01-18 20:01:46 +0800

    review

--- springboot-simple-demos/src/main/java/com/example/controller/SingletonController.java
+++ springboot-simple-demos/src/main/java/com/example/controller/SingletonController.java
@@ -1,6 +1,6 @@
 package com.example.controller;
 
-import com.example.common.UserSingletonService;
+import com.example.service.UserSingletonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
