767160f91311b823858459d6e30a8e7e5d3f5510
zhougaojun <zhougaojun@dxy.cn>
2021-07-01 15:00:20 +0800

    springboot之actuator健康检查使用实例(http://localhost:8080/actuator/health)

---
+++ springboot-actuator/README.md
@@ -0,0 +1,32 @@
+# springboot-actuator
+springboot健康检查
+
+## 验证健康检查
+启动项目访问：http://localhost:8080/actuator/health
+```json
+{
+  "status": "DOWN",
+  "components": {
+    "diskSpace": {
+      "status": "UP",
+      "details": {
+        "total": 250685575168,
+        "free": 81286369280,
+        "threshold": 10485760
+      }
+    },
+    "ping": {
+      "status": "UP"
+    },
+    "rocketMQ": {
+      "status": "DOWN",
+      "details": {
+        "errorCode": 500
+      }
+    }
+  }
+}
+
+```
+## 获取自定义的检查参数
+http://localhost:8080/actuator/info
--- springboot-actuator/src/main/java/com/example/RocketMqHealthIndicator.java
+++ springboot-actuator/src/main/java/com/example/RocketMqHealthIndicator.java
@@ -4,11 +4,12 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-/** 自定义健康检查
+/** 自定义健康检查，模拟RocketMq监控检查
+ *  启动服务访问：http://localhost:8080/actuator/health
  * @author jameszhou
  */
 @Component
-public class RocketMQHealthIndicator implements HealthIndicator {
+public class RocketMqHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
         int errorCode = check();
@@ -20,6 +21,7 @@ public class RocketMQHealthIndicator implements HealthIndicator {
     }
 
     private int check(){
+        // 返回 500 模拟检查检查失败
         return 500;
     }
 }
