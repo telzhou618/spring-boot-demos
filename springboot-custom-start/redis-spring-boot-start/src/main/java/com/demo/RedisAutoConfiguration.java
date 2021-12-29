4f68529d851cc39000771c08d4923bfa1d31fd21
zhougaojun <zhougaojun@dxy.cn>
2021-12-29 11:43:45 +0800

    自定义start

--- springboot-custom-start/redis-spring-boot-start/src/main/java/com/demo/RedisAutoConfiguration.java
+++ springboot-custom-start/redis-spring-boot-start/src/main/java/com/demo/RedisAutoConfiguration.java
@@ -4,11 +4,14 @@ import com.demo.config.RedisProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import redis.clients.jedis.Jedis;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author zhougaojun
@@ -16,8 +19,22 @@ import redis.clients.jedis.Jedis;
 @Slf4j
 @Configuration
 @ConditionalOnClass(Jedis.class)
+/**
+ * 使得配置RedisProperties生效
+ */
 @EnableConfigurationProperties(RedisProperties.class)
+/**
+ * 存在 redis.enable 配置，且值为true时生效
+ * havingValue 存在且值和havingValue的值相等时生效
+ * matchIfMissing 不存在时的默认值
+ */
+@ConditionalOnProperty(name = "redis.enable", havingValue = "true", matchIfMissing = false) //  RedisAutoConfiguration 生效条件
 public class RedisAutoConfiguration {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("RedisAutoConfiguration.init...");
+    }
 
     @Bean
     @ConditionalOnMissingBean
--- springboot-custom-start/redis-spring-boot-start/src/main/java/com/demo/config/RedisProperties.java
+++ springboot-custom-start/redis-spring-boot-start/src/main/java/com/demo/config/RedisProperties.java
@@ -11,9 +11,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @Setter
 @ToString
+/**
+ * 配置文件 指定前缀，配合@EnableConfigurationProperties生效
+ */
 @ConfigurationProperties(prefix = "redis")
 public class RedisProperties {
 
+
+    private Boolean enable = true;
     private String host = "127.0.0.1";
     private int port = 6379;
     private String password;
