bd85f3deb4bb091b32edea19b9393c02f3c74725
zhougaojun <zhougaojun@dxy.cn>
2023-01-04 15:28:03 +0800

    springboot open-feign 实例

--- springboot-open-feign-demo/src/test/java/com/example/SpringbootOpenFeignDemoApplicationTests.java
+++ springboot-open-feign-demo/src/test/java/com/example/SpringbootOpenFeignDemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class SpringbootOpenFeignDemoApplicationTests {
 
+    @Resource
+    private JSONPlaceHolderClient jsonPlaceHolderClient;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(JSON.toJSONString(jsonPlaceHolderClient.getPosts(), true));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(JSON.toJSONString(jsonPlaceHolderClient.getPostById(1L), true));
     }
 
 }
